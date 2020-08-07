092720b2a2ec156ab269fe15a5a952d79923ddcd
Will Gonzalez <will.colocho@gmail.com>
2020-08-07 04:12:02 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
  count = 0 
  new_nums = []
   while array[count] do 
-   new_nums<< (array[count] ** 2)
+   new_nums.push(array[count] ** 2)
    count += 1 
   end
   new_nums
